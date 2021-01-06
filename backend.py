3dffd53331432b4656e33cae6e03be779ff0bcad
mikub97 <mikub97@gmail.com>
2021-01-06 22:48:45 +0100

    legend and prototype of patient info added

--- backend.py
+++ backend.py
@@ -19,44 +19,48 @@ users = get_users(c)
 sensors = ["L0","L1","L2","R0","R1","R2",]
 patient_picker_options = []
 for user in users:
-    patient_picker_options.append({'label': str(user['firstname'] + " " + user['secondname']), 'value': user['secondname']})
+    patient_picker_options.append({'label': str(user['firstname'] + " " + user['secondname']), 'value':user['secondname']})
 
-app = dash.Dash(__name__,assets_url_path="assets") #Here we implement the app but for now smth really basic is included
+app = dash.Dash(__name__,assets_url_path="assets",) #Here we implement the app but for now smth really basic is included
 
 app.layout = html.Div([
     html.H1("Real time plot"),
-    dcc.Graph(
-        id = "real-time-plot",
-        animate = True,
-    ),
+
+    html.Div([
+        dcc.Dropdown(id='patient-picker', options=patient_picker_options, value=users[0]['secondname']),
+        html.Label("............................................................................"),
+        html.Label("PATIENT'S BASIC INFORMATION :"),
+        html.Label("............................................................................"),
+        html.Label(" NAME : JACEK "),
+        html.Label(" SURNAME : TRZASKOWSKI "),
+        html.Label(" IS DISABLED? : YES "),
+        html.Label(" BIRTHDAY : 1333 "),
+    ], className="three columns"),
+    html.Div([
+        html.Label("............................................................................"),
+        html.Label("----------STATISTICAL DATA-------------:"),
+        html.Label("............................................................................"),
+        html.Label(" L0 AVG = 23423423 "),
+        html.Label(" L1 AVG = 23423423 "),
+        html.Label(" L2 AVG = 23423423 "),
+        html.Label(" etc... "),
+    ], className="five columns"),
+    html.Img(src=app.get_asset_url('table_foot.png'),className="img"),
+    html.Div(dcc.Graph(id = "real-time-plot",animate = True)),
+    # html.Div([
+    #             html.Img(src=app.get_asset_url('table.png'),className="img"),
+    #             html.Img(src=app.get_asset_url('foot.png'), className="img")
+    #         ],className="three columns"),
     dcc.Interval(
         id = 'plot-update',
         interval = 1000
     ),
-    dcc.Dropdown(id='patient-picker', options=patient_picker_options, value="Grzegorczyk"),
     html.Div([
-
-        html.Div([                          ## PATIENT DETAILS
-            html.Label('Name: '),
-            html.Div(id='patient-firstname'),
-            html.Label('Surname'),
-            html.Div(id='patient-secondname'),
-            html.Label('Birthdate'),
-            html.Div(id='patient-birtdate'),
-            html.Label('Is the patient disabled?'),
-            html.Div(id='patient-disabled'),
-
-        ],id="patient-detail",title="Details",className="six columns"),
-
-
-        html.Div([                              ## CONTROLS
+        html.Div([
+            html.Label("          PLACE FOR MANY OTHER CONTROL BUTTONS ETC.            "),
             html.Label('How many last values?'),
-            dcc.Slider(id='time-period', value=20, min=10, max=40, step=1),
-            html.Div(id='boolean-switch-output'),
-            daq.BooleanSwitch(id='my-boolean-switch',on=False),
-            html.Label('Interval settings:'),
-
-        ],className="six columns")
+            dcc.Slider(id='time-period', value=20, min=10, max=40, step=1)
+            ] ,className="six columns")
     ])
 ])
 
@@ -76,15 +80,15 @@ def update_output(on):
              Input('patient-picker', 'value'),
              Input("time-period",'value')])
 def update(data,selected_patient,time_period):
-    c.execute("SELECT TRACETIME,L0,L1,L2,R0,R1,R2 FROM 'traces' WHERE SECONDNAME = '"+selected_patient+"' ORDER BY TRACETIME DESC ")
+    c.execute("SELECT TRACETIME,L0,L1,L2,R0,R1,R2 FROM 'traces' WHERE SECONDNAME = '"+str(selected_patient)+"' ORDER BY TRACETIME DESC ")
     rows = c.fetchall()
     dataSQL = [list(row) for row in rows]
     labels = ["T","L0",'L1','L2','R0','R1','R2']
     df = pd.DataFrame.from_records(dataSQL, columns=labels)
     data = []
     for s in sensors:
-        X = df["T"][:30]
-        Y = df[s][:30]
+        X = df["T"][:time_period]
+        Y = df[s][:time_period]
 
         data.append(go.Scatter(
             x = list(X),
@@ -102,7 +106,7 @@ def update(data,selected_patient,time_period):
 @app.callback(Output("patient-firstname","children"),
              [Input('patient-picker', 'value')])
 def update(selected_patient):
-    return "dsadas"
+    return "fasdlkjflkdsamlkfslfslnfdslknfdsa"
 
 
 app.run_server()
