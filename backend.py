08bbd724a9d221b6deb0adfadd8b89c2c141e879
mikub97 <mikub97@gmail.com>
2021-01-07 11:37:13 +0100

    styling

--- backend.py
+++ backend.py
@@ -23,20 +23,31 @@ for user in users:
 
 app = dash.Dash(__name__,assets_url_path="assets",) #Here we implement the app but for now smth really basic is included
 
+#################################################### MENU ####################################################
+
+menu_div = html.Div([
+    html.Div([html.Span(html.H3("WALKING PROCESS ANALYSIS"),className="logo")],className="three columns"),
+    html.Div([html.Span(html.H3("STATISTICAL DATA"))], className="seven columns"),
+    html.Div([html.Span(html.H3("LEGEND"))],className="two columns")
+
+],className="twelve columns")
+
+
+
+#################################################### PATIENT INFO, STATISTICAL DATA, AND THE PICTURE OF FEET ####################################################
+
 statistical_vs = ['avg','min','max','sth',"some",'avg1','min1','max1','sth1',"some1",'avg2','min2','max2','sth2',"some2",'avg3','min3','max3','sth3',"some3",'avg4','min4','max4','sth4',"some4"]
 nrows=5
 patient_info = ["firstname","secondname",'birthdate','is disabled?']
-app.layout = html.Div([
-    html.H1("Real time plot"),
-    html.Div([
+
+patient_div =html.Div([
         html.Div([
-            dcc.Dropdown(id='patient-picker', options=patient_picker_options, value=users[0]['secondname']),
+            dcc.Dropdown(id='patient-picker', options=patient_picker_options, value=users[0]['secondname'], placeholder="Select a user",),
             html.H3("PATIENT INFO"),
             html.Table(
                 ([html.Tr([html.Td(val), html.Td(val + "_v", id=val + "_v")]) for val in patient_info]))
         ], className="three columns"),
         html.Div([
-            html.Span(html.H3("STATISTICAL DATA")),
             html.Div([
                 html.Div(
                     html.Table(
@@ -66,19 +77,61 @@ app.layout = html.Div([
 
         ], className="seven columns"),
         html.Div([html.Img(src=app.get_asset_url('table.png'),className="img"),html.Img(src=app.get_asset_url('foot.png'),className="img")],className="two columns")
-    ],className="patient_details"),
+    ],className="patient_details")
+
+
+#################################################### CONTROL AREA - BUTTONS, SLIDERS AND SO ON ####################################################
+
+control_div = html.Div([
+        html.Div([
+            html.Label('How many last values?'),
+            dcc.Slider(id='time-period', value=20, min=10, max=40, step=1),
+            html.Label('Refresh rate'),
+            dcc.Slider(id='refresh_slider', value=20, min=10, max=40, step=1),
+            html.Label("Real time values"),
+            daq.BooleanSwitch(id='real_switch',on=True)] ,className="four columns"),
+        html.Div([
+            html.Div([
+                html.Table(
+                        [html.Tr([html.Td("From when?"),html.Td(dcc.Input(id="from"))]),
+                          html.Tr([html.Td("Till when"), html.Td(dcc.Input(id="till"))]),]),
+                html.Button('Submit(online in non-real-time)', id='submit-historical'),
+            ],className="six columns"),
+
+            html.Div([
+                html.Table(
+                    [html.Tr([html.Td("blablabdsla"), html.Td(dcc.Input(id="bldsa"))]),
+                     html.Tr([html.Td("blablabla"), html.Td(dcc.Input(id="blabla"))]),
+                     ]),
+                html.Button('Submit blabla', id='submit-blabla'),
+            ], className="six columns")
+        ],className="eight columns")
+        # html.Div([
+        #     html.Button('Real-Time Values', id='real-time-run'),
+        #     html.Label("Refresh time"),
+        #     dcc.Input(id="refresh_time"),
+        #     html.Label("From when?"),
+        #     dcc.Input(id="from"),
+        #     html.Label("Until when?"),
+        #     dcc.Input(id="until"),
+        #     html.Button('Submit(online in non-real-time)', id='submit-historical')], className="three columns")
+])
+
+
+
+#################################################### APPLICATION LAYOUT ####################################################
+
+
+
+app.layout = html.Div([
+    menu_div,
+    patient_div,
     html.Div(dcc.Graph(id = "real-time-plot",animate = True),className="twelve columns"),
     dcc.Interval(
         id = 'plot-update',
         interval = 1000
     ),
-    html.Div([
-        html.Div([
-            html.Label("          PLACE FOR MANY OTHER CONTROL BUTTONS ETC.            "),
-            html.Label('How many last values?'),
-            dcc.Slider(id='time-period', value=20, min=10, max=40, step=1)
-            ] ,className="six columns")
-    ])
+   control_div
 ])
 
 
